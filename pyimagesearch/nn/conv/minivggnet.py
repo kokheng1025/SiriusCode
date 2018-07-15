5bd96df0472faeb4a33b681d96237d9888a260fc
kokheng <kokhenglai1025@gmail.com>
2018-07-15 21:08:54 +0800

    monitoring training process

--- pyimagesearch/nn/conv/minivggnet.py
+++ pyimagesearch/nn/conv/minivggnet.py
@@ -27,20 +27,20 @@ class MiniVGGNet:
         model.add(Conv2D(32, (3, 3), padding="same", 
             input_shape=inputShape))
         model.add(Activation("relu"))
-        #model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chanDim))
         model.add(Conv2D(32, (3, 3), padding="same"))
         model.add(Activation("relu"))
-        #model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
         # second CONV => RELU => CONV => RELU => POOL layer set
         model.add(Conv2D(64, (3, 3), padding="same"))
         model.add(Activation("relu"))
-        #model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chanDim))
         model.add(Conv2D(64, (3, 3), padding="same"))
         model.add(Activation("relu"))
-        #model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
         
